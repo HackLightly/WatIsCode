94219233fc506fa4b4aae0a25d42d699fb1101c7
rageandqq <chitley.sameer@gmail.com>
2014-11-21 21:55:12 -0500

    Added basic rules.

--- compiler.cc
+++ compiler.cc
@@ -1,20 +1,65 @@
-#include <stdio> 
-#include <regex>
+#include <iostream> 
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
-using std::cout;
-using std::cin;
-using std::ifstream;
-using std::ofstream;
-using std::endl;
-using std::string;
+using namespace std;
+
+
+vector<string> splitString (string input) {
+ 	istringstream iss(input); 
+ 	vector<string> tokens; 
+ 	copy(istream_iterator<string>(iss), 
+		istream_iterator<string>(), 
+		back_inserter(tokens)); 
+	return tokens; 
+ }
+
+string ummRemover (string line) {
+	int found = line.find("UMM");
+	if (found != string::npos) {
+		line = line.substr(0, found);
+	}
+	return line;
+}
 
 int main (int argc, char* argv[]) {
 	string line;
-	while (getline(cin, line)) {
-		if (line == "") continue;
-		
-		
+	int lineNum = 0;
+	try {
+		while (getline(cin, line)) {
+			lineNum++;
+			if (line == "") continue;
+			line = ummRemover(line); //ignore comments
+			vector<string> tokens = splitString(line); //tokenize with space as delimiter
+					
+			
+			if (tokens.size() > 3 && tokens.at(0) == "DO" && tokens.at(1) == "I" &&
+				tokens.at(2) == "NEED") {
+				cout << "#include <" << tokens.at(3) << ">" << endl;
+			}
+			else if (tokens.size() > 5 && tokens.at(0) == "I" && tokens.at(1) == "NEED" &&
+					tokens.at(2) == "A" && tokens.at(4) == "CALLED") {
+				cout << tokens.at(3) << " " << tokens.at(5) << ";" << endl;
+			}
+			else if (tokens.size() > 3 && tokens.at(1) == "SHOULD" && tokens.at(2) == "BE") {
+				cout << tokens.at (0) << " = ";
+				for (int i = 3; i < tokens.size(); i++) {
+					cout << tokens.at(i);
+				}
+				cout << ";" << endl;
+			}
+			else if (tokens.size() > 2 && tokens.at(0) == "ASK" && tokens.at(1) == "ABOUT") {
+				cout << "cin >> " << tokens.at(3) << endl;
+			}
+			else {
+				throw string(line);
+			}
+		}
+	}
+	catch (string &msg) {
+		cerr << "ERROR: Line number " << lineNum << ": \"" << msg << "\" is unsupported." << endl;
 	}
 }
+
