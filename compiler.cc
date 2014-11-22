58bdbe08326a9c73c34c24a6b428e2ff04c640fa
rageandqq <chitley.sameer@gmail.com>
2014-11-21 21:10:10 -0500

    Project Initialized.

---
+++ compiler.cc
@@ -0,0 +1,20 @@
+#include <stdio> 
+#include <regex>
+#include <string>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::ifstream;
+using std::ofstream;
+using std::endl;
+using std::string;
+
+int main (int argc, char* argv[]) {
+	string line;
+	while (getline(cin, line)) {
+		if (line == "") continue;
+		
+		
+	}
+}
