11d58bba23b00e0e4a9e6e2b0e4535ccb40d5306
rageandqq <chitley.sameer@gmail.com>
2014-11-21 23:01:27 -0500

    Removed error catch, fixed indenting

--- compiler.cc
+++ compiler.cc
@@ -27,116 +27,111 @@ string ummRemover (string line) {
 int main (int argc, char* argv[]) {
   string line;
   int lineNum = 0;
-  try {
-    while (getline(cin, line)) {
-      lineNum++;
-      if (line == "") continue;
-      line = ummRemover(line); //ignore comments
-      vector<string> tokens = splitString(line); //tokenize with space as delimiter
+  while (getline(cin, line)) {
+    lineNum++;
+    if (line == "") continue;
+    line = ummRemover(line); //ignore comments
+    vector<string> tokens = splitString(line); //tokenize with space as delimiter
 
-      //INCLUDE
-      if (tokens.size() > 3 && tokens.at(0) == "DO" && tokens.at(1) == "I" &&
-          tokens.at(2) == "NEED") {
-        cout << "#include <" << tokens.at(3) << ">" << endl;
+    //INCLUDE
+    if (tokens.size() > 3 && tokens.at(0) == "DO" && tokens.at(1) == "I" &&
+        tokens.at(2) == "NEED") {
+      cout << "#include <" << tokens.at(3) << ">" << endl;
+    }
+    //DECLARATION
+    else if (tokens.size() > 5 && tokens.at(0) == "I" && tokens.at(1) == "NEED" &&
+        tokens.at(2) == "A" && tokens.at(4) == "CALLED") {
+      cout << tokens.at(3) << " " << tokens.at(5) << ";" << endl;
+    }
+    //ASSIGNMENT
+    else if (tokens.size() > 3 && tokens.at(1) == "SHOULD" && tokens.at(2) == "BE") {
+      cout << tokens.at (0) << " = ";
+      for (int i = 3; i < tokens.size(); i++) {
+        cout << tokens.at(i) << " ";
       }
-      //DECLARATION
-      else if (tokens.size() > 5 && tokens.at(0) == "I" && tokens.at(1) == "NEED" &&
-          tokens.at(2) == "A" && tokens.at(4) == "CALLED") {
-        cout << tokens.at(3) << " " << tokens.at(5) << ";" << endl;
+      cout << ";" << endl;
+    }
+    //INPUT
+    else if (tokens.size() > 2 && tokens.at(0) == "ASK" && tokens.at(1) == "ABOUT") {
+      cout << "std::cin >> " << tokens.at(2)  << ";" << endl;
+    }
+    //OUTPUT
+    else if (tokens.size() > 2 && tokens.at(0) == "SHOW" && tokens.at(1) == "ME") {
+      cout << "std::cout << ";
+      for (int i = 2; i < tokens.size(); i++) {
+        cout << tokens.at(i) << " ";
       }
-      //ASSIGNMENT
-      else if (tokens.size() > 3 && tokens.at(1) == "SHOULD" && tokens.at(2) == "BE") {
-        cout << tokens.at (0) << " = ";
-        for (int i = 3; i < tokens.size(); i++) {
-          cout << tokens.at(i) << " ";
-        }
-        cout << ";" << endl;
-      }
-      //INPUT
-      else if (tokens.size() > 2 && tokens.at(0) == "ASK" && tokens.at(1) == "ABOUT") {
-        cout << "std::cin >> " << tokens.at(2)  << ";" << endl;
-      }
-      //OUTPUT
-      else if (tokens.size() > 2 && tokens.at(0) == "SHOW" && tokens.at(1) == "ME") {
-        cout << "std::cout << ";
-        for (int i = 2; i < tokens.size(); i++) {
-          cout << tokens.at(i) << " ";
-        }
-        cout << " << std::endl;" << endl;
-      }
-      //WHILE
-      else if (tokens.size() == 3 && tokens.at(0) == "WATER" && tokens.at(1) == "WATER" && tokens.at(2) == "WATER") {
-        cout << "while (true) {" << endl;
-      }
-      //BREAK
-      else if (tokens.size() == 3 && tokens.at(0) == "RUN" && tokens.at(1) == "FROM" && tokens.at(2) == "GEESE") {
-        cout << "break;" << endl;
-      }
-      //END LOOP
-      else if (tokens.size() == 3 && tokens.at(0) == "LOOP" && tokens.at(1) == "LOOP" && tokens.at(2) == "LOOP") {
-        cout << "}" << endl;
-      }
-      //FUNCTION DECLARATION
-      else if (tokens.size() > 3 && tokens.at(1) == "FINDS" && tokens.at(2) == "A") {
-        if (tokens.size() > 4 && tokens.at(4) == "FROM" && tokens.at(5) == "A" && tokens.at(7) == "CALLED") {
-          cout << tokens.at(3) << " " << tokens.at(0) << "(";
-          for (int i = 4; i < tokens.size(); i+= 5) {
-            cout << tokens.at(i + 2) << " " << tokens.at(i + 4);
-            if (i + 5 < tokens.size()) {
-              cout << ", ";
-            }
+      cout << " << std::endl;" << endl;
+    }
+    //WHILE
+    else if (tokens.size() == 3 && tokens.at(0) == "WATER" && tokens.at(1) == "WATER" && tokens.at(2) == "WATER") {
+      cout << "while (true) {" << endl;
+    }
+    //BREAK
+    else if (tokens.size() == 3 && tokens.at(0) == "RUN" && tokens.at(1) == "FROM" && tokens.at(2) == "GEESE") {
+      cout << "break;" << endl;
+    }
+    //END LOOP
+    else if (tokens.size() == 3 && tokens.at(0) == "LOOP" && tokens.at(1) == "LOOP" && tokens.at(2) == "LOOP") {
+      cout << "}" << endl;
+    }
+    //FUNCTION DECLARATION
+    else if (tokens.size() > 3 && tokens.at(1) == "FINDS" && tokens.at(2) == "A") {
+      if (tokens.size() > 4 && tokens.at(4) == "FROM" && tokens.at(5) == "A" && tokens.at(7) == "CALLED") {
+        cout << tokens.at(3) << " " << tokens.at(0) << "(";
+        for (int i = 4; i < tokens.size(); i+= 5) {
+          cout << tokens.at(i + 2) << " " << tokens.at(i + 4);
+          if (i + 5 < tokens.size()) {
+            cout << ", ";
           }
-          cout << ") {" << endl;
-        }
-        else {
-          cout << tokens.at(3) << " " << tokens.at(0) << "() {" << endl;
-        }
-      }
-      //FUNCTION END
-      else if (tokens.size() > 1 && tokens.at(0) == "PLEASE" && tokens.at(1) == "WORK") {
-        cout << "}" << endl;
-      }
-      //IF
-      else if (tokens.size() > 2 && tokens.at(0) == "I" && tokens.at(1) == "THINK") {
-        cout << "if (";
-        for (int i = 2; i < tokens.size(); i++) {
-          cout << tokens.at(i) << " ";
         }
         cout << ") {" << endl;
-      }
-      //ELSE IF
-      else if (tokens.size() > 2 && tokens.at(0) == "OR" && tokens.at(1) == "MAYBE") {
-        cout << "} else if (";
-        for (int i = 2; i < tokens.size(); i++) {
-          cout << tokens.at(i) << " ";
-        }
-        cout << ") {" << endl;
-      }
-      //ELSE
-      else if (tokens.size() == 2 && tokens.at(0) == "I" && tokens.at(1) == "LIED") {
-        cout << "} else {" << endl;
-      }
-      //END IF
-      else if (tokens.size() == 1 && tokens.at(0) == "RIGHT?") {
-        cout << "}" << endl;
-      }
-      //RETURN
-      else if (tokens.size() > 2 && tokens.at(0) == "I" && tokens.at(1) == "CALCULATED") {
-        cout << "return ";
-        for (int i = 2; i < tokens.size(); i++) {
-          cout << tokens.at(i) << " ";
-        }
-        cout << ";" << endl;
       }
       else {
-        // Anything else (function call)
-        cout << line << ";" << endl;
+        cout << tokens.at(3) << " " << tokens.at(0) << "() {" << endl;
       }
     }
+    //FUNCTION END
+    else if (tokens.size() > 1 && tokens.at(0) == "PLEASE" && tokens.at(1) == "WORK") {
+      cout << "}" << endl;
+    }
+    //IF
+    else if (tokens.size() > 2 && tokens.at(0) == "I" && tokens.at(1) == "THINK") {
+      cout << "if (";
+      for (int i = 2; i < tokens.size(); i++) {
+        cout << tokens.at(i) << " ";
+      }
+      cout << ") {" << endl;
+    }
+    //ELSE IF
+    else if (tokens.size() > 2 && tokens.at(0) == "OR" && tokens.at(1) == "MAYBE") {
+      cout << "} else if (";
+      for (int i = 2; i < tokens.size(); i++) {
+        cout << tokens.at(i) << " ";
+      }
+      cout << ") {" << endl;
+    }
+    //ELSE
+    else if (tokens.size() == 2 && tokens.at(0) == "I" && tokens.at(1) == "LIED") {
+      cout << "} else {" << endl;
+    }
+    //END IF
+    else if (tokens.size() == 1 && tokens.at(0) == "RIGHT?") {
+      cout << "}" << endl;
+    }
+    //RETURN
+    else if (tokens.size() > 2 && tokens.at(0) == "I" && tokens.at(1) == "CALCULATED") {
+      cout << "return ";
+      for (int i = 2; i < tokens.size(); i++) {
+        cout << tokens.at(i) << " ";
+      }
+      cout << ";" << endl;
+    }
+    else {
+      // Anything else (function call)
+      cout << line << ";" << endl;
+    }
   }
-
-  catch (string &msg) {
-    cerr << "ERROR: Line number " << lineNum << ": \"" << msg << "\" is unsupported." << endl;
   }
 }
 
